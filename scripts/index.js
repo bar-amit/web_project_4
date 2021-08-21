bcd1f3452ab2ab1c9a91c0f5b95a2d3ed4381fd9
bar-amit <bar.amit@outlook.com>
2021-08-21 17:56:56 +0300

    refactor(index.js): style and minor changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,49 @@
-// Let's find the buttons in the DOM
-let editButton = document.querySelector('.profile__edit-button');
-let closeButtons = document.querySelectorAll('.popup__close-button');
-let addButton = document.querySelector('.profile__add-button');
+/*
+  DOM elements:
+*/
 
-// Let's get the input elements in the DOM
-let nameInput = document.querySelector('.popup__input_type_name');
-let bioInput = document.querySelector('.popup__input_type_bio');
-let titleInput = document.querySelector('.popup__input_type_title');
-let linkInput = document.querySelector('.popup__input_type_link');
+// Buttons
+const editButton = document.querySelector('.profile__edit-button');
+const closeButtons = document.querySelectorAll('.popup__close-button');
+const addButton = document.querySelector('.profile__add-button');
 
-// Select elements where the field values will be entered
-let profileName = document.querySelector('.profile__name');
-let profileBio = document.querySelector('.profile__bio');
+// Inputs
+const nameInput = document.querySelector('.popup__input_type_name');
+const bioInput = document.querySelector('.popup__input_type_bio');
+const titleInput = document.querySelector('.popup__input_type_title');
+const linkInput = document.querySelector('.popup__input_type_link');
 
-// Popup elements
-let popupEdit = document.querySelector('.popup_name_edit-profile');
-let popupNew = document.querySelector('.popup_name_new-place');
-let popupPicture = document.querySelector('.popup_name_picture');
+// Profile
+const profileName = document.querySelector('.profile__name');
+const profileBio = document.querySelector('.profile__bio');
 
-// Let's find the forms in the DOM
-let formProfile = document.querySelector('.popup__form_name_edit-profile');
-let formPlace = document.querySelector('.popup__form_name_new-place');
+// Popups
+const popupEdit = document.querySelector('.popup_name_edit-profile');
+const popupNew = document.querySelector('.popup_name_new-place');
+const popupPicture = document.querySelector('.popup_name_picture');
+
+// Forms
+const formProfile = document.querySelector('.popup__form_name_edit-profile');
+const formPlace = document.querySelector('.popup__form_name_new-place');
 
 // Cards container
-let cardsContainer = document.querySelector('.gallery__container');
+const cardsContainer = document.querySelector('.gallery__container');
 
-// Template
-let cardTemplate = document.querySelector('.card__template').content;
+// Card template
+const cardTemplate = document.querySelector('.card__template').content;
+
+/*
+  Function handlers:
+*/
 
 // Close click handler
 function handleClose(e) {
-
-  // Now, let's hide
   e.target.closest('.popup').classList.remove('popup_visible');
 }
 
-// Add click handler
-function handleAdd() {
-  popupNew.classList.add('popup_visible');
+// Delete click handler
+function handleDelete(e) {
+  e.target.closest('.card').remove();
 }
 
 // Like click handle
@@ -45,33 +51,32 @@ function handleLike(e) {
   e.target.classList.toggle('card__like-button_active');
 }
 
+// Add click handler
+function handleAdd() {
+  popupNew.classList.add('popup_visible');
+}
+
 // Edit click handler
 function handleEdit() {
-
-  // Let's get the name's and bio's current text and fill the form
+  // Fill the form with current values
   nameInput.value = profileName.textContent;
   bioInput.value = profileBio.textContent;
 
-  // Now, let's show to the world our popup
   popupEdit.classList.add('popup_visible');
 }
 
-// Next is the form submit handler, though
-// it won't submit anywhere just yet
+// Profile submit handler
 function handleProfileSubmit(evt) {
-  evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
-  // Having done so, we can define our own way of submitting the form.
-  // We'll explain it in more detail later.
+  evt.preventDefault();
 
-  // Get the values of each field from the corresponding value property
-  // Insert new values using the textContent property of the querySelector() method
+  // Get the values of each field and insert new values
   profileName.textContent = nameInput.value;
   profileBio.textContent = bioInput.value;
 
-  // Our work here is done, why won't we close that pop-up then?
   evt.target.closest('.popup').classList.remove('popup_visible');
 }
 
+// Place submit handler
 function handlePlaceSubmit(evt) {
   evt.preventDefault();
 
@@ -80,11 +85,10 @@ function handlePlaceSubmit(evt) {
   evt.target.closest('.popup').classList.remove('popup_visible');
 }
 
-function handleDelete(e) {
-  e.target.closest('.card').remove();
-}
+/*
+  Cards:
+*/
 
-// Cards' data:
 const initialCards = [
   {
     name: "Yosemite Valley",
@@ -112,28 +116,30 @@ const initialCards = [
   }
 ];
 
+// Add new card to gallery
 function addCard(card){
-  let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-  let cardTitle = cardElement.querySelector('.card__title');
-  let cardImage = cardElement.querySelector('.card__image');
-  let cardLikeButton = cardElement.querySelector('.card__like-button');
-  let cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardLikeButton = cardElement.querySelector('.card__like-button');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
 
   cardTitle.textContent = card.name;
   cardImage.setAttribute('src',card.link);
   cardImage.setAttribute('alt',card.name);
-  cardImage.addEventListener('click', getPictureEventHandle(card));
+  cardImage.addEventListener('click', getPictureEventHandler(card));
   cardLikeButton.addEventListener('click',handleLike);
   cardDeleteButton.addEventListener('click',handleDelete);
 
   cardsContainer.prepend(cardElement);
 }
 
-function getPictureEventHandle(card) {
-  let pictureHandle = function() {
-    let pictureTitle = popupPicture.querySelector('.popup__picture-title');
-    let pictureImage = popupPicture.querySelector('.popup__picture');
+// Create an event handler for each new card
+function getPictureEventHandler(card) {
+  const handlePicture = function() {
+    const pictureTitle = popupPicture.querySelector('.popup__picture-title');
+    const pictureImage = popupPicture.querySelector('.popup__picture');
 
     pictureTitle.textContent = card.name;
     pictureImage.setAttribute('src',card.link);
@@ -141,15 +147,21 @@ function getPictureEventHandle(card) {
 
     popupPicture.classList.add('popup_visible');
   }
-  return pictureHandle;
+  return handlePicture;
 }
 
 // Load cards
 initialCards.forEach(card=>addCard(card));
 
-// Connect the event handlers to the elements:
+/*
+  Connect the event handlers to the elements
+*/
+
+// Forms
 formProfile.addEventListener('submit', handleProfileSubmit);
 formPlace.addEventListener('submit', handlePlaceSubmit);
+
+// Buttons
 closeButtons.forEach(button=>button.addEventListener('click', handleClose));
 editButton.addEventListener('click', handleEdit);
 addButton.addEventListener('click', handleAdd);
