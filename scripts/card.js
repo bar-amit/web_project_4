e0ac12a5dd130b7c975f23e08e9996620741ceb4
bar-amit <bar.amit@outlook.com>
2021-09-21 20:10:55 +0300

    refactor(vars): moved all the vars to  a new module

--- scripts/card.js
+++ scripts/card.js
@@ -1,25 +1,25 @@
 class Card {
-  constructor(data, selectors) {
+  constructor(data, assets) {
 
     //data
     this._name = data.name;
     this._link = data.link;
 
     // selectors
-    this._cardSelector = selectors.cardSelector;
-    this._cardTemplateSelector = selectors.cardTemplateSelector;
-    this._cardTitleSelector = selectors.cardTitleSelector;
-    this._cardImageSelector = selectors.cardImageSelector;
-    this._cardLikeSelector = selectors.cardLikeSelector;
-    this._cardDeleteSelector = selectors.cardDeleteSelector;
+    this._cardSelector = assets.cardSelector;
+    this._cardTemplateSelector = assets.cardTemplateSelector;
+    this._cardTitleSelector = assets.cardTitleSelector;
+    this._cardImageSelector = assets.cardImageSelector;
+    this._cardLikeSelector = assets.cardLikeSelector;
+    this._cardDeleteSelector = assets.cardDeleteSelector;
 
     // bound handlers:
     this._handleLike = this._handleLikeClick.bind(this);
     this._handleDelete = this._handleCardDelete.bind(this);
 
-    this._handlePicture = selectors.openPicture;
+    this._handlePicture = assets.openPicture;
 
-    this._activelikeButtonClass = selectors.activelikeButtonClass;
+    this._activelikeButtonClass = assets.activelikeButtonClass;
   }
 
   _getTemplate() {
@@ -35,8 +35,6 @@ class Card {
     const likeButton = this._element.querySelector(this._cardLikeSelector);
     likeButton.classList.toggle(this._activelikeButtonClass);
   }
-
-
 
   _addCardEvents(){
     const cardImage = this._element.querySelector(this._cardImageSelector);
